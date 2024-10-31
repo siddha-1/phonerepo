4a883f67e042dfe6554d1ae473893927bd984b51
siddha-1 <samaresh0b1@gmail.com>
2024-10-31 12:44:49 -0700

    Create ganesh.cpp
    Jai Shri Ganesh!

---
+++ ganesh.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namesspace std;
+
+int main(int argc, char* argv[]) 
+{
+
+  cout << "Jai Shri Ganesh !" << endl;
+  cout << "Jai Maa Mahalaxmi!" << endl;
+
+  return 0;
+}
